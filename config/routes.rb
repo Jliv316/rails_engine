187fe07ff5e6841859b5e8070611f4b961f05360
Jliv316 <jclivingston316@gmail.com>
2018-06-26 17:36:12 -0600

    adds multiple invoice record tests

--- config/routes.rb
+++ config/routes.rb
@@ -4,18 +4,19 @@ Rails.application.routes.draw do
       namespace :merchants do
         get '/find', to: "search#show"
         get '/find_all', to: "search#index"
+        get '/customers_with_pending_invoices', to:"invoice_analysis#index"
+      end
+
+      namespace :invoices do
+        get '/find', to: "search#show"
+        get '/find_all', to: "search#index"
+      end
+      
+      resources :invoices, only: [:index, :show] do
+        resources :transactions, only: [:index, :show]
       end
 
       resources :merchants, only: [:index, :show]
     end
   end
-
-  namespace :api do
-    namespace :v1 do
-      resources :invoices, only: [:index, :show] do
-        resources :transactions, only: [:index, :show]
-      end
-    end
-  end
-  
 end
