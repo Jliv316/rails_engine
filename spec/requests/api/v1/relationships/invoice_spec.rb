43463e8aec6d855089249a08acce39fdb0fd7f96
Tyler Westlie <tyler.westlie@gmail.com>
2018-06-26 18:35:08 -0600

    WIP invoice/invoice_items relationship

--- spec/requests/api/v1/relationships/invoice_spec.rb
+++ spec/requests/api/v1/relationships/invoice_spec.rb
@@ -13,4 +13,14 @@ describe 'relationships' do
     transactions_json = JSON.parse(response.body)
     expect(invoices[0].transactions.count).to eq(transactions_json.count)
   end
+
+  it 'returns a collection of transactions' do
+    invoices = create_list(:invoice, 5)
+    create(:invoice_item, invoice: invoices[0])
+    create(:invoice_item, invoice: invoices[0])
+
+    get "/api/v1/invoices/#{invoices[0].id}/invoice_items"
+
+    expect(response).to be_successful
+  end
 end
