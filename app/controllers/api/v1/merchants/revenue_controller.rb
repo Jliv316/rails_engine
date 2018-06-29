32d40749eebe7ef26e06baf2cd84826b2850fda4
Jliv316 <jclivingston316@gmail.com>
2018-06-28 20:59:42 -0600

    adds BI merchant

--- app/controllers/api/v1/merchants/revenue_controller.rb
+++ app/controllers/api/v1/merchants/revenue_controller.rb
@@ -1,5 +1,13 @@
 class Api::V1::Merchants::RevenueController < ApplicationController
   def show
-    render json: Merchant.find(params[:merchant_id]).single_merchant_revenue, serializer: MerchantRevenueSerializer
+    if params[:date]
+      render json: Merchant.find(params[:merchant_id]).merchant_revenue_by_date(date), serializer: MerchantRevenueSerializer
+    else
+      render json: Merchant.find(params[:merchant_id]).single_merchant_revenue, serializer: MerchantRevenueSerializer
+    end
+  end
+
+  def index
+    render json: Merchant.merchants_revenue_by_date(date), serializer: MerchantRevenueSerializer
   end
 end
--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -12,6 +12,22 @@ class Merchant < ApplicationRecord
           .where(transactions: {result: "success"})[0].revenue
   end
 
+  def merchant_revenue_by_date(date)
+    invoices
+          .select("sum(invoice_items.unit_price * invoice_items.quantity) AS revenue")
+          .joins(:invoice_items, :transactions)
+          .where(transactions: {result: "success"}, created_at: date.beginning_of_day..date.end_of_day)[0]
+          .revenue
+  end
+
+  def self.merchants_revenue_by_date(date)
+    Invoice
+          .select("sum(invoice_items.unit_price * invoice_items.quantity) AS revenue")
+          .joins(:invoice_items, :transactions)
+          .where(transactions: {result: "success"}, created_at: date.beginning_of_day..date.end_of_day)[0]
+          .revenue
+  end
+
   def self.most_items(quantity)
     joins(:invoices, :transactions,:invoice_items)
     .order("sum(invoice_items.quantity) DESC")
