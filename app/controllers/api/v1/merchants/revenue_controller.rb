b3b74ac055d6630beaab806241b2ebdfc94def24
Tyler Westlie <tyler.westlie@gmail.com>
2018-06-28 20:21:34 -0600

    passes rspec and spec harness for single merchant revenue for single day

--- app/controllers/api/v1/merchants/revenue_controller.rb
+++ app/controllers/api/v1/merchants/revenue_controller.rb
@@ -1,7 +1,7 @@
 class Api::V1::Merchants::RevenueController < ApplicationController
   def show
     if params["date"]
-      render json: Merchant.find(params[:merchant_id]).single_merchant_revenue_by_date(params["date"]), serializer: MerchantRevenueSerializer
+      render json: Merchant.find(params[:merchant_id]).single_merchant_revenue_by_date(params["date"].to_date), serializer: MerchantRevenueSerializer
     else
       render json: Merchant.find(params[:merchant_id]).single_merchant_revenue, serializer: MerchantRevenueSerializer
     end
